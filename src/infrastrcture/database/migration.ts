a6960c9079d700927170bb8be6b2bbfaa25a277d
Rafael Amaral Naves Avelar <rafael.amaral@monocard.com.br>
2024-01-14 12:29:36 -0300

    fix: new eslint standard

--- src/infrastrcture/database/migration.ts
+++ src/infrastrcture/database/migration.ts
@@ -1,10 +1,10 @@
-const pgp = require('pg-promise')();
-import { IDatabase } from 'pg-promise';
-import { join } from 'path';
+import { IDatabase } from 'pg-promise'
+import { join } from 'path'
+const pgp = require('pg-promise')()
 export class Migration {
-    public static async generateTables(db: IDatabase<any>): Promise<void> {
-        const filePath = join(__dirname, 'sql-queries', 'create-tables.sql');
-        const query = new pgp.QueryFile(filePath);
-        await db.query(query);
-    }
+  public static async generateTables(db: IDatabase<any>): Promise<void> {
+    const filePath = join(__dirname, 'sql-queries', 'create-tables.sql')
+    const query = new pgp.QueryFile(filePath)
+    await db.query(query)
+  }
 }
--- src/utils/Error.ts
+++ src/utils/Error.ts
@@ -1,8 +1,8 @@
 export class Error {
-    errorMessage: string;
-    errorStatus: number;
-    constructor({ errorMessage, errorStatus }) {
-        this.errorMessage = errorMessage;
-        this.errorStatus = errorStatus;
-    }
+  errorMessage: string
+  errorStatus: number
+  constructor({ errorMessage, errorStatus }) {
+    this.errorMessage = errorMessage
+    this.errorStatus = errorStatus
+  }
 }
