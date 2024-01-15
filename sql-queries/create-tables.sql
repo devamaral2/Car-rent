aa0d146c705f6f9e0de90ca6445893b405ac8d18
Rafael Amaral Naves Avelar <rafael.amaral@monocard.com.br>
2024-01-15 01:56:54 -0300

    chore: new database

--- sql-queries/create-tables.sql
+++ sql-queries/create-tables.sql
@@ -1,6 +1,20 @@
 CREATE TABLE IF NOT EXISTS Automoveis (
-  id SERIAL PRIMARY KEY AUTO_INCREMENT,
+  id SERIAL PRIMARY KEY,
   placa varchar(7) NOT NULL,
   marca varchar(100) NOT NULL,
   cor varchar(100) NOT NULL
-) 
+); 
+
+CREATE TABLE IF NOT EXISTS Motoristas (
+  id SERIAL PRIMARY KEY,
+  nome varchar(255) NOT NULL
+); 
+
+CREATE TABLE IF NOT EXISTS Alugueis (
+  id SERIAL PRIMARY KEY,
+  descricao varchar(255) NOT NULL,
+  data_inicio date NOT NULL,
+  data_termino date NOT NULL,
+  id_automovel integer REFERENCES Automoveis(id),
+  id_motorista integer REFERENCES Motoristas(id)
+);
