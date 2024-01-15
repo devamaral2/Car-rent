38ede0d315fa213378c9630d4ba787ac1c589cc5
Rafael Amaral Naves Avelar <rafael.amaral@monocard.com.br>
2024-01-14 22:06:57 -0300

    refactor: using serial to automovel table

--- sql-queries/create-tables.sql
+++ sql-queries/create-tables.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS Automoveis (
-  id INT PRIMARY KEY,
+  id SERIAL PRIMARY KEY AUTO_INCREMENT,
   placa varchar(7) NOT NULL,
   marca varchar(100) NOT NULL,
   cor varchar(100) NOT NULL
